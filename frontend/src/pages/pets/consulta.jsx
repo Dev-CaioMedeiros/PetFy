0f98999bf83bd234819f9a1fcf37128217ed4017
Dev-CaioMedeiros <Devcaiomedeiros@gmail.com>
2025-12-02 18:48:18 -0300

    fix images error

--- frontend/src/pages/pets/consulta.jsx
+++ frontend/src/pages/pets/consulta.jsx
@@ -2,7 +2,7 @@ import { ArrowLeft, Search, ChevronRight, Clock } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import "../../styles/pets/consultas.css";
 import { useState } from "react";
-import promoImg from "../../assets/pet_consulta.png";
+
 
 export default function Consultas() {
   const navigate = useNavigate();
@@ -67,7 +67,7 @@ export default function Consultas() {
           </button>
         </div>
 
-        <img src="promoImg" 
+        <img src="../../assets/pet_consulta.png" 
         className="c-promo-img" />
       </div>
 
