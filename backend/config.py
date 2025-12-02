a8cc2ec0a65c48f7a43d89fa67798feadfa8fbcd
Dev-CaioMedeiros <Devcaiomedeiros@gmail.com>
2025-12-02 15:07:21 -0300

    fix db_port again

--- backend/config.py
+++ backend/config.py
@@ -5,7 +5,6 @@ from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 
-# Carrega variáveis do .env
 load_dotenv()
 
 db = SQLAlchemy()
@@ -14,20 +13,24 @@ bcrypt = Bcrypt()
 def init_app():
     app = Flask(__name__)
 
-    # Configuração do banco
     DB_USER = os.getenv("DB_USER")
     DB_PASSWORD = os.getenv("DB_PASSWORD")
     DB_HOST = os.getenv("DB_HOST")
     DB_NAME = os.getenv("DB_NAME")
-    DB_PORT = int(os.getenv("DB_PORT", 3306))
+
+    DB_PORT = os.getenv("DB_PORT")
+
+    if not DB_PORT:
+        DB_PORT = 3306
+    else:
+        DB_PORT = int(DB_PORT)
 
     app.config['SQLALCHEMY_DATABASE_URI'] = \
-    f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+        f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
     
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.secret_key = os.getenv('SECRET_KEY', 'dev123')
 
-    # Inicializações
     db.init_app(app)
     bcrypt.init_app(app)
     CORS(app)
