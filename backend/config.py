d8374a36c754736d8755eff92f2e6f9a58e98b59
Dev-CaioMedeiros <Devcaiomedeiros@gmail.com>
2025-12-02 15:04:09 -0300

    fix db_port

--- backend/config.py
+++ backend/config.py
@@ -19,7 +19,7 @@ def init_app():
     DB_PASSWORD = os.getenv("DB_PASSWORD")
     DB_HOST = os.getenv("DB_HOST")
     DB_NAME = os.getenv("DB_NAME")
-    DB_PORT = os.getenv("DB_PORT", 3306)
+    DB_PORT = int(os.getenv("DB_PORT", 3306))
 
     app.config['SQLALCHEMY_DATABASE_URI'] = \
     f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
